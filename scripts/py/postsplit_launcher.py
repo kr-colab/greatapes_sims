41df06598b5b1d0365845890c4853e0ed12b2190
mufernando <murillofer.rodrigues@gmail.com>
2019-09-16 14:28:37 -0700

    first commit

---
+++ scripts/py/postsplit_launcher.py
@@ -0,0 +1,39 @@
+import subprocess
+import os
+from sim_funcs import *
+
+slurm=True
+table_path ="/home/murillor/projects/greatapes_sims/meta/param_sims_spp.csv"
+meta_path = "/home/murillor/projects/greatapes_sims/meta/sims/"
+reps = 10
+out_path = "/home/murillor/projects/greatapes_sims/output/"
+
+script_path = "/home/murillor/projects/greatapes_sims/scripts/slim/neutral_split_greatapes.slim"
+burnin_path = "/home/murillor/projects/greatapes_sims/output/neut_burn_unmut_RAND_R3MVZ1633Q.trees"
+prefix = "neut_split"
+prefix_mut = "neut_split_mut"
+
+mutate=1e-9
+gens = tb.gens.unique()
+
+for i in range(gens):
+    for j in range(reps):
+        rand = id_generator()
+        var_names = ["N1","N2", "path_burnin", "gens", "mu", "r", "L", "RAND"]
+        values = ["10000","10000", burnin_path, gens[i], "0","1.5e-8","50818468",rand]
+
+        write_sim_sh(var_names, values, prefix, out_path ,script_path, meta_path, rand, mutate, prefix_mut)
+        if (slurm):
+            cmd = "sbatch "+rand+".sh"
+        else:
+            cmd = "sh "+rand+".sh"
+        out = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE)
+
+        if (out.returncode != 0):
+            print("sh failed")
+            print(out)
+        else:
+            jid = out.stdout.decode().strip().split(" ")[-1]
+            write_meta(meta_path, var_names, values, rand, jid)
+
+        os.remove(rand+".sh")
