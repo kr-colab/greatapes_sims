2ae86f4ad0c54bc593ff354b1f168f836c806fd7
mufernando <murillofer.rodrigues@gmail.com>
2020-01-25 20:04:31 -0800

    added reps
    Former-commit-id: 2099e6474a6729627843281320bff4b9a7a576f5

--- scripts/py/stats_from_tree.py
+++ scripts/py/stats_from_tree.py
@@ -6,20 +6,23 @@ import sys
 import os.path
 from stats_funcs import *
 
-print("input: tree_path(str), out_path(str), spp(str), rand_id(str), win_size(int), L(int), n(int)")
-assert len(sys.argv) == 8, "Not enough input was provided."
+print("input: tree_path(str), filename(str), spp(str), rand_id(str), rep(str), win_size(int), L(int), n(int)")
+assert len(sys.argv) == 9, "Not enough input was provided."
 
 ts_path = sys.argv[1]
-out_path = sys.argv[2]
+filename = sys.argv[2]
 spp = sys.argv[3]
 rand_id = sys.argv[4]
-win_size = int(sys.argv[5])
-L = int(sys.argv[6])
-n = int(sys.argv[7])
+rep = sys.argv[5]
+win_size = int(sys.argv[6])
+L = int(sys.argv[7])
+n = int(sys.argv[8])
+print(rand_id)
+print(n)
 stats = single_pop_stats_from_ts(ts_path, L, win_size, n)
 stats['spp'] = spp
 stats['rand_id'] = rand_id
-filename=out_path+spp+"_single_stats_"+rand_id+".tsv"
+stats['rep'] = rep
 stats.to_csv(filename, sep="\t", header=(not os.path.exists(filename)), index=False, mode="a")
 
 
