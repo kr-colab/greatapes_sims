2a74a8c56bcaba28bdb80c51a51f7a7088bcd125
Murillo Fernando Rodrigues <murillofer.rodrigues@gmail.com>
2020-03-31 10:16:19 -0300

    option to do one window in the center of the chr
    Former-commit-id: 70b41b0dc5a54f4d28adc5bcc764bfe3b90bc867

--- scripts/py/stats_from_tree.py
+++ scripts/py/stats_from_tree.py
@@ -6,8 +6,10 @@ import sys
 import os.path
 from stats_funcs import *
 import numpy as np
-print("input: tree_path(str), filename(str), spp(str), rand_id(str), rep(str), win_size(int), L(int), n(int)")
-assert len(sys.argv) == 9, "Not enough input was provided."
+print("input: tree_path(str), filename(str), spp(str), rand_id(str), rep(str), win_size(int), L(int), n(int), center(bool)")
+# if center is True, then stats will be calculated only for one window center in the chr L
+assert len(sys.argv) < 9, "Not enough input was provided."
+center=False
 ts_path = sys.argv[1]
 filename = sys.argv[2]
 spp = sys.argv[3]
@@ -16,9 +18,11 @@ rep = sys.argv[5]
 win_size = int(sys.argv[6])
 L = int(sys.argv[7])
 n = int(sys.argv[8])
+if len(sys.argv > 9:
+    center = True
 print(rand_id)
 print(n)
-stats = single_pop_stats_from_ts(ts_path, L, win_size, n)
+stats = single_pop_stats_from_ts(ts_path, L, win_size, n, center)
 stats['spp'] = spp
 stats['rand_id'] = rand_id
 stats['rep'] = rep
