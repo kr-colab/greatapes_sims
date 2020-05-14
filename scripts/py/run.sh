9d875ab9f42b9c7105ce18c7dc5b47677a802fe0
mufernando <murillofer.rodrigues@gmail.com>
2020-05-14 09:52:18 -0700

    --jobs option in snakemake behaves weirdly when greater than 100
    Former-commit-id: 22e4f43ce5d623d98f938c62d64d22bc6338a47f

--- scripts/py/run.sh
+++ scripts/py/run.sh
@@ -1,5 +1,5 @@
 conda activate greatapes
 module use /projects/apps/shared/modulefiles/
-module load SLiM/dev bedtools
-snakemake -p --snakefile chr12.snake --profile slurm --jobs 2000 --cluster-config talapas.yaml
-snakemake -p --snakefile windowed.snake --profile slurm --jobs 2000 --cluster-config talapas.yaml
+module load SLiM bedtools
+snakemake -p --snakefile chr.snake --profile slurm --jobs 100 --cluster-config talapas.yaml
+snakemake -p --snakefile windowed.snake --profile slurm --jobs 100 --cluster-config talapas.yaml
