c586ba7eeba03f0f5a33faff0ef9cb3343886a29
mufernando <murillofer.rodrigues@gmail.com>
2020-11-02 10:11:56 -0800

    changing snakemake options
    Former-commit-id: 81380932bcf2ddb245ea0e3a849ec57dd41d8328

--- scripts/py/run.sh
+++ scripts/py/run.sh
@@ -1,4 +1,4 @@
 conda activate greatapes
 module use /projects/apps/shared/modulefiles/
 module load SLiM bedtools
-snakemake -p --snakefile ga_sims.snake --profile slurm --jobs 1000 --cluster-config talapas.yaml
+snakemake -p --snakefile ga_sims.snake --profile slurm
