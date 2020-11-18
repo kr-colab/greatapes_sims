390fdd6443eb1872fdc241d63767d8e6d43a064b
mufernando <murillofer.rodrigues@gmail.com>
2020-11-18 14:35:51 -0800

    minor
    Former-commit-id: dc8277ddd43dee7210eae6089976ddff92180329

--- scripts/py/run.sh
+++ scripts/py/run.sh
@@ -1,4 +1,5 @@
-conda activate greatapes
+#srun -p kern -A kernlab --mem 8GB --ntasks 2 --time 25-00:00:00 --pty bash
+conda activate apes
 module use /projects/apps/shared/modulefiles/
 module load SLiM bedtools
-snakemake -p --snakefile ga_sims.snake --profile slurm
+snakemake -p --snakefile ga_sims.snake --profile slurm --restart-times 3
