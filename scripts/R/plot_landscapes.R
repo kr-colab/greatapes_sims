d2a07ddba9dc9e82f213d4ae3f70456cae18bbb4
Murillo R <murillofer.rodrigues@gmail.com>
2021-06-17 10:48:13 -0700

    now pis are added to tmrca = 0 in the plot of tmrca vs correls
    Former-commit-id: b832159e2b62265e4417377312c27f39eaa2c18f [formerly b7569ea1b75d62cf7675c935614515f9289f5bb0]
    Former-commit-id: 63c42753a25915daa7285005a6a6c6c13685f212

--- scripts/R/plot_landscapes.R
+++ scripts/R/plot_landscapes.R
@@ -53,15 +53,18 @@ np <- reticulate::import("numpy", convert=T)
 print("igothere")
 
 neutsup="O814WK8MN3UOF27JIR"
-wbgsonlysup="RD24T8SVUEW8FTANCW"
-sbgsonlysup="4GROMS6FHMNW5X9L72"
-filepath = "../../output/joined_stats/sup-rand-id_O814WK8MN3UOF27JIR_rep_0_win-size_1000000_sample-size_10.npz"
+sbgssup="4GROMS6FHMNW5X9L72"
+sbgsspossup="GGKXIAL2IXDKAJYXAN"
+filepath = paste0("../../output/joined_stats/sup-rand-id_", sbgsspossup,"_rep_0_win-size_1000000_sample-size_10.npz")
 
 
 parser <- ArgumentParser(description='Get plots from npz file with dxy matrix, labels and windows')
 parser$add_argument('npzfilepath',  type="character")
 
 filepath = parser$parse_args()$npzfilepath
+filepath=filepath
+
+
 
 outpath = "../../output/figs/"
 edges = fread("../../meta/edges_meta.tsv", sep="\t", header=T, fill=T)
@@ -70,8 +73,6 @@ edges$parent = str_replace(edges$parent, "_", "-")
 sims_info = read.table("../../output/sims_info.tsv", sep="\t")
 header = read.table("../../output/header_sims_info.tsv", header=T)
 colnames(sims_info) = colnames(header)
-
-sup_rand_id = neutsup
 sup_rand_id = strsplit(filepath, "_")[[1]][3]
 rep = strsplit(filepath, "_")[[1]][5]
 win_size = as.integer(strsplit(filepath, "_")[[1]][7])
@@ -117,7 +118,7 @@ setkey(mrcas, mrca)
 setkey(edges, edge)
 mrcas = mrcas[edges[,c("edge", "ancestral_coding")],]
 dxy=mrcas[dxy, on=.(combo)]
-dxy$combo = factor(dxy$combo, levels = mrcas$combo[order(mrcas$combo!=mrcas$mrca, mrcas$tmrca, mrcas$ancestral_coding)], labels=str_replace(mrcas$combo[order(mrcas$combo!=mrcas$mrca, mrcas$tmrca, mrcas$ancestral_coding)], "_", " ")) # levels ordered by within vs between spp comparison, tmrca, and ancestral coding (tree order)
+dxy$combo = factor(dxy$combo, levels = mrcas$combo[order(ifelse(mrcas$combo==mrcas$mrca, 0.1, 1)*mrcas$ancestral_coding, mrcas$tmrca)], labels=str_replace(mrcas$combo[order(ifelse(mrcas$combo==mrcas$mrca, 0.1, 1)*mrcas$ancestral_coding, mrcas$tmrca)], "_", " ")) # levels ordered by within vs between spp comparison, tmrca, and ancestral coding (tree order)
 u=unique(mrcas[,c("mrca", "ancestral_coding", "tmrca")])
 dxy$mrca = factor(dxy$mrca, levels=unique(u$mrca[order(u$tmrca, u$ancestral_coding)]))
 # joining with rec rate and getting mean rate by window
@@ -134,18 +135,59 @@ dxy=dxy[, .(ex_overlap = sum(percent)), by=setdiff(colnames(dxy), c("start.y", "
 setnames(dxy, c("start.x", "end.x"), c("start","end"))
 dxy[, ex_overlap := 100*ex_overlap]
 
-dxy[,.(cor = cor(value, mean_rec, use="complete.obs")), by=.(spp1, spp2)]
-dxy[,.(cor = cor(value, ex_overlap, use="complete.obs")), by=.(spp1, spp2)]
+## REMOVING 0 rec rate window
+dxy=dxy[dxy$mean_rec!=0,]
+
+dxy[,.(cor = cor(value, mean_rec, use="complete.obs", method="spearman")), by=.(spp1, spp2)]
+dxy[,.(cor = cor(value, ex_overlap, use="complete.obs", method="spearman")), by=.(spp1, spp2)]
 
 cor_rec = dxy %>%
     group_by(spp1,spp2) %>% 
-    do(tidy(cor.test(.$value, .$mean_rec)))
+    do(tidy(cor.test(.$value, .$mean_rec, method="spearman")))
 
 cor_ex = dxy %>%
     group_by(spp1,spp2) %>% 
-    do(tidy(cor.test(.$value, .$ex_overlap)))
+    do(tidy(cor.test(.$value, .$ex_overlap, method="spearman")))
 
-ggplot(data=dxy, aes(y=value, x=ex_overlap, col=mean_rec)) + geom_point() + facet_wrap(vars(combo), scales="free_y", labeller = label_wrap_gen(width=18)) + theme_bw() + labs(y="Pi/Dxy", x="% Exon", col="Rec rate", title=spaced_desc) + scale_colour_viridis_c(direction=-1)
+
+#### HUMANS ONLY###
+
+humans = dxy[dxy$spp1=="humans"|dxy$spp2=="humans",]
+humans$combo=droplevels(humans$combo)
+humans$combo = factor(humans$combo, levels=c("humans", "central-chimp humans", "eastern-chimp humans", "humans nigerian-chimp", "humans western-chimp", "bonobo humans", "eastern-gorilla humans", "humans western-gorila", "bornean-orangutan humans", "humans sumatran-orangutan"), labels=c("humans", "humans central-chimp", "humans eastern-chimp", "humans nigerian-chimp", "humans western-chimp", "humans bonobo", "humans eastern-gorilla", "humans western-gorila", "humans bornean-orangutan", "humans sumatran-orangutan"))
+drop_subspp = c("humans eastern-chimp", "humans nigerian-chimp", "humans western-chimp", "humans bonobo", "humans eastern-gorilla","humans bornean-orangutan")
+humans=humans[!humans$combo %in% drop_subspp,]
+humans$combo=droplevels(humans$combo)
+ggplot(data=humans, aes(y=value, x=ex_overlap, col=mean_rec)) + geom_point(size=1) + facet_wrap(vars(combo), scales="free_y", labeller = label_wrap_gen(width=18)) + theme_bw(base_size=24) + labs(y="Pi and Dxy", x="% Exon", col="Rec rate", subtitle=desc) + scale_colour_viridis_c(option="magma", direction=-1)
+ggsave(filename=paste0(outpath,"exon-scatter-humans-subset_",desc,"_",sup_rand_id,".pdf"), width = 20, height = 4, units = "in")
+
+ggplot(data=humans, aes(y=value, x=mean_rec, col=ex_overlap)) + geom_point(size=1) + facet_wrap(vars(combo), scales="free_y", labeller = label_wrap_gen(width=18), nrow=1) + theme_bw(base_size=24) + labs(y="Pi and Dxy", x="Rec rate", col="% Exon", subtitle=desc) + scale_colour_viridis_c(direction=-1)
+ggsave(filename=paste0(outpath,"rec-scatter-humans-subset_",desc,"_",sup_rand_id,".pdf"), width = 20, height = 4, units = "in")
+
+col5palette = c("#b96440", "#864bc6", "#749c51", "#b24c82","#6d81b0")
+droplevs = c("western-chimp", "nigerian-chimp", "eastern-chimp", "eastern-gorilla", "bornean-orangutan")
+sub=dxy[(!dxy$combo %in% droplevs) & (dxy$stat=="pi"),]
+sub$combo = droplevels(sub$combo)
+p_pi = ggplot(data = sub, aes(x=start, y=value, group=combo)) + geom_line(aes(col=combo)) + scale_colour_manual(values=col5palette) + theme_bw(base_size=24) + labs(y="Diversity", x="Window", col="Species") + guides(colour = guide_legend(override.aes = list(size=5))) + theme(legend.position = "bottom")
+p_pi
+ggsave(filename=paste0(outpath,"pi-landscape_subset_chr12_",desc,"_",sup_rand_id,".pdf"), width = 14, height = 7, units = "in")
+
+t=dcast(dxy[dxy$stat=="pi" & !(dxy$spp1 %in% droplevs) & !(dxy$spp2 %in% droplevs) ,], formula=start+end~combo, value.var='value')
+cormat=cor(t[,-c("start", "end")], use="complete.obs", method="spearman")
+library(ggtree)
+library(colorspace)
+tree <- read.tree(text = "(((humans:3765,(central-chimp:871,bonobo:871):2894):1854,western-gorila:5669):5519,sumatran-orangutan:11138);")
+tree$edge.length=(tree$edge.length*1000)/20
+ptips=ggtree(tree, ladderize=F) + geom_tiplab(size=4) + geom_treescale(x=0,y=0,width=100000)
+gheatmap(ptips, cormat, offset=340000, low="white", high="black", colnames_position = "bottom", font.size=4, colnames_angle=45, colnames_offset_y=-.2, width=1.2) + scale_fill_continuous_diverging(palette="Blue-Red 3",limits=c(-1,1)) + labs(fill="Corr")
+
+ggplot(data = reshape2::melt(cormat), aes(x=Var1, y=Var2, fill=value)) + geom_tile() + scale_fill_continuous_diverging(palette="Blue-Red 3",limits=c(-1,1))+ theme_minimal(base_size=14) + labs(title="Corr between Pi/Dxy",subtitle=spaced_desc, fill="Corr") + xlab("") + ylab("")  + theme(axis.text.x = element_text(angle = 45, vjust = 1.1, hjust=1))
+ggsave(filename=paste0(outpath,"pi-cormat_genome-wide_subset_",desc,"_",sup_rand_id,".pdf"), width = 5.8, height = 5.2, units = "in")
+
+########
+
+
+ggplot(data=dxy, aes(y=value, x=ex_overlap, col=mean_rec)) + geom_point() + facet_wrap(vars(combo), scales="free_y", labeller = label_wrap_gen(width=18)) + theme_bw() + labs(y="Pi/Dxy", x="% Exon", col="Rec rate", title=spaced_desc) + scale_colour_viridis_c(option="magma", direction=-1)
 ggsave(filename=paste0(outpath,"exon-scatter_",desc,"_",sup_rand_id,".pdf"), width = 500, height = 400, units = "mm")
 
 ggplot(data=dxy, aes(y=value, x=mean_rec, col=ex_overlap)) + geom_point() + facet_wrap(vars(combo), scales="free_y", labeller = label_wrap_gen(width=18)) + theme_bw() + labs(y="Pi/Dxy", x="Rec rate", col="% Exon", title=spaced_desc) + scale_colour_viridis_c(direction=-1)
@@ -156,6 +198,7 @@ p_ex = ggplot(cor_ex, aes(x=p.value)) + geom_histogram() + theme_bw() + labs(y="
 p_rec = ggplot(cor_rec, aes(x=p.value)) + geom_histogram() + theme_bw() + labs(y="Count", x="P-value", title="Correlation between pi/dxy and recombination rate")
 
 (p_ex / p_rec) + plot_annotation(title=spaced_desc)
+
 
 ggsave(filename=paste0(outpath,"pval-dist_",desc,"_",sup_rand_id,".pdf"), width = 210, height = 297, units = "mm")
 
@@ -179,6 +222,8 @@ coly2 = "#7a387c"
 p_rec_ex = ggplot(data=dxy[dxy$combo=="humans",], aes(x=start)) + geom_line(aes(y=mean_rec), col=coly1) + geom_line( aes(y=ex_overlap / 2), col=coly2) + scale_y_continuous(name = "Rec rate", sec.axis = sec_axis(~.*2, name="% Exon")) + theme_bw(base_size = 12) + theme(axis.title.y = element_text(color = coly1), axis.title.y.right = element_text(color = coly2))  + annotate("text", label=paste0("Corr = ", round(cor_rec_ex$estimate, 2)), x=1e8, y = 5.5) + xlab("Window") + theme(legend.margin=margin(t=0, r=0, b=0, l=-1, unit="cm"))
 
 (p_pi / p_dxy_mrca / p_rec_ex) + plot_annotation(title=spaced_desc, theme=theme(title = element_text(size=8))) + plot_layout(heights=c(3,3,1))
+
+(p_pi / p_dxy_mrca) + plot_annotation(title=spaced_desc)
 
 ggsave(filename=paste0(outpath,"pidxy-landscape_",desc,"_",sup_rand_id,".pdf"), width = 210, height = 297, units = "mm")
 
