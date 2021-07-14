1392b1094ccd2b94fac4e132281d2002fb623f65
mufernando <murillofer.rodrigues@gmail.com>
2021-07-14 15:55:52 -0700

    minor

--- scripts/py/clear_temp_trees.py
+++ scripts/py/clear_temp_trees.py
@@ -1,3 +1,11 @@
+"""
+Clears temporary tree sequence files related to the specified final tree sequence path.
+It assumes the temporary files to be removed have the same file name as the final ts, but with a numeric suffix.
+e.g.,
+Final tree sequence path: /home/result_sim.trees
+Temporary files to be removed: /home/result_sim.trees10, /home/result_sim.trees100
+"""
+
 import sys
 import glob
 import os
