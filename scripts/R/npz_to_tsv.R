86ea267c86bef3a8232b8ce2328aa4c1d42b58a6
mufernando <murillofer.rodrigues@gmail.com>
2021-10-20 11:11:42 -0700

    minor fixes

--- scripts/R/npz_to_tsv.R
+++ scripts/R/npz_to_tsv.R
@@ -10,26 +10,26 @@ use_python("/opt/anaconda3/envs/apes/bin/python")
 
 np <- reticulate::import("numpy", convert=T)
 
-neutsup="O814WK8MN3UOF27JIR"
+#neutsup="O814WK8MN3UOF27JIR"
 
-filepath = paste0("../../output/joined_stats/sup-rand-id_", neutsup,"_rep_0_win-size_1000000_sample-size_10.npz")
+#infilepath = paste0("../../output/joined_stats/sup-rand-id_", neutsup,"_rep_0_win-size_1000000_sample-size_10.npz")
 
 
 parser <- ArgumentParser(description='Get plots from npz file with dxy matrix, labels and windows')
 parser$add_argument('npzfilepath',  type="character", help="Path to .npz file")
-parser$add_argument('outpath',  type="character", help="Output path")
+parser$add_argument('outfilepath',  type="character", help="Path to out .tsv file")
 
 
-filepath = parser$parse_args()$npzfilepath
-outpath = parser$parse_args()$outpath
+infilepath = parser$parse_args()$npzfilepath
+outfilepath = parser$parse_args()$outfilepath
 
 
 colorder=c("value", "stat", "spp1", "spp2", "chr", "start", "end", "n_acc_bases")
 
-sup_rand_id = strsplit(filepath, "_")[[1]][3]
-win_size = as.integer(strsplit(filepath, "_")[[1]][7])
+win_size = as.integer(strsplit(infilepath, "_")[[1]][7])
+## TODO: add the correct extension and edit the wreite.table below!!
 
-npz = np$load(filepath)
+npz = np$load(infilepath)
 npz$files
 dxy_mat = npz$f[["dxy"]]
 coord_starts = npz$f[["coord_windows"]]
@@ -41,7 +41,7 @@ colnames(dxy_mat) = paste0("col", 1:ncol(dxy_mat))
 rownames(labels) = paste0("col", 1:ncol(dxy_mat)) #row in labels is col in dxy_mat
 colnames(labels) = c("spp1", "spp2")
 names(coord_starts) = paste0("row", 1:nrow(dxy_mat))
-coords = data.table(rn = names(coord_starts), chr=chroms, start=coord_starts, end = coord_starts+win_size)
+coords = data.table(rn = names(coord_starts), chr=chroms, start=coord_starts, end = coord_starts+win_size) #idk why I didn't use coord_ends?
 labels = data.table(labels, variable=rownames(labels))
 dxy = melt(data.table(dxy_mat, keep.rownames = TRUE), id.vars = c("rn"))
 
@@ -53,4 +53,5 @@ dxy$start = as.integer(dxy$start)
 dxy$end = as.integer(dxy$end)
 dxy$n_acc_bases = dxy$end - dxy$start
 
-write_table(dxy[,colorder], paste0(outpath,sup_rand_id,"-pidxy_win-size",win_size,".tsv"), sep="\t", quote=FALSE, row.names=FALSE)
+setcolorder(dxy, colorder)
+write.table(dxy, outfilepath, sep="\t", quote=FALSE, row.names=FALSE)
