011ec206099f1a9e5a161ace787e2310295d7750
mufernando <murillofer.rodrigues@gmail.com>
2021-11-15 13:51:12 -0800

    fix

--- scripts/py/stats_from_ts.py
+++ scripts/py/stats_from_ts.py
@@ -1,6 +1,4 @@
 import tskit
-import pyslim
-import msprime
 import dendropy
 import glob
 import numpy as np
@@ -25,19 +23,18 @@ coords_dict = eval(args['coords_dict'])
 
 # Loading tree sequence and list with populations
 assert os.path.exists(args["infilepath"]) and os.path.exists(args["popsfilepath"]), f"Trees file or .pops file does not exist: {args['infilepath']}, {args['popsfilepath']}"
-print("about to load")
-recap_tsu = pyslim.load(args["infilepath"])
+print("about to load", flush=True)
+recap_tsu = tskit.load(args["infilepath"])
 with open(args["popsfilepath"], "r") as f:
     pops = eval(f.readline())
-print("finished loading")
+print("finished loading", flush=True)
 rng = np.random.default_rng(args['seed'])
 # getting contemporary samples
 # note the time of "contemporary" samples varies bc of differences in generation times
 # TODO: sample individuals not nodes
-pop_samples = [recap_tsu.samples(population_id=i+1) for i in range(len(pops))]
-contemp_time = [np.min(recap_tsu.tables.nodes.time[samples]) for samples in pop_samples]
-contemp_samples = [rng.choice(pop_samples[pid][recap_tsu.tables.nodes.time[pop_samples[pid]] == contemp_time[pid]], args["sample_size"], replace=False) for pid in range(len(pop_samples))]
-print("samples selected")
+contemp_times = [np.min(recap_tsu.tables.nodes.time[recap_tsu.samples(population_id=i+1)]) for i in range(len(pops))]
+contemp_samples = [rng.choice(recap_tsu.samples(population_id=i+1, time=contemp_times[i]), args["sample_size"], replace=False) for i in range(len(pops))]
+print("samples selected", flush=True)
 
 # windowing
 start = coords_dict['start']-coords_dict['padded_start']
@@ -55,9 +52,9 @@ if start > 0:
     windows = np.insert(windows, 0, [0])
 if expected_length > stop:
     windows = np.append(windows, [expected_length])
-print(recap_tsu.sequence_length)
-print(start, stop)
-print(windows, coord_windows)
+print(recap_tsu.sequence_length, flush=True)
+print(start, stop, flush=True)
+print(windows, coord_windows, flush=True)
 # obtaining indexes for all possible pairs (including diversity, i.e. i==j for (i,j))
 indexes = [(x, y) for x in range(len(pops)) for y in range(len(pops)) if x >= y]
 
@@ -68,8 +65,8 @@ assert dxy.shape[1] == ((len(pops)**2 - len(pops))/2) + len(pops)
 
 # getting the species labels
 labels = np.array([[pops[i],pops[j]] for i, j in indexes])
-print(indexes)
-print(labels)
+print(indexes, flush=True)
+print(labels, flush=True)
 # slicing out the padded windows
 if start > 0:
     dxy = dxy[1:]
@@ -80,7 +77,7 @@ if expected_length > stop:
 
 # creating array with chrom name
 chrom = np.full(shape=dxy.shape[0], fill_value=coords_dict['chr'])
-print(dxy.shape, windows.shape, coord_windows.shape)
+print(dxy.shape, windows.shape, coord_windows.shape, flush=True)
 assert (dxy.shape[0]+1) == windows.shape[0] == coord_windows.shape[0]
 
 # saving to output
