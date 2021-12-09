a144e0e242b63fb454e95fd646a4dbfccc38e476
mufernando <murillofer.rodrigues@gmail.com>
2021-12-09 13:42:51 -0800

    nolock

--- scripts/sims/run.sh
+++ scripts/sims/run.sh
@@ -1,2 +1,3 @@
 conda activate apes
-snakemake -p --snakefile ga_sims.snake --profile slurm --restart-times 3 --latency-wait 30 --configfile partialsweeps.yaml
+snakemake -p --snakefile ga_sims.snake --profile slurm --restart-times 6 --latency-wait 30 --configfile partialsweeps.yaml --nolock
+snakemake -p --snakefile ga_sims.snake --profile slurm --restart-times 6 --latency-wait 30 --configfile chr12_strongbgs.yaml --nolock
